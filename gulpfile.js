fc0ea60ce46f0a87141686c6a45e8dba7247e082
Jon McLean <jon@poweredbyrawon.com>
2021-03-25 11:13:54 +0000

    Added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,70 @@
 /* jshint esversion: 6 */
 
-/* node */
-var gulp  = require('gulp');
-var path  = require('path');
-var color = require('cli-color');
+'use strict';
+
+/* package */
+let gulp = require('gulp');
 
 /* gulp */
-var sass   = require('gulp-sass');
-var rename = require('gulp-rename');
-var reload = require('gulp-livereload');
-var prefix = require('gulp-autoprefixer');
-var jshint = require('gulp-jshint');
-var shell  = require('gulp-shell');
-var map    = require('gulp-sourcemaps');
+let sass   = require('gulp-sass');
+let rename = require('gulp-rename');
+let reload = require('gulp-livereload');
+let prefix = require('gulp-autoprefixer');
+let map    = require('gulp-sourcemaps');
 
-/* glob */
-var glob = {
-    scss: ['./src/scss/**/*.scss'],
-    script: ['./src/script/**/*.js',]
+/* logging */
+let log = require('log'); 
+require('log-node')();
+
+/* watch */
+let watch = {
+	scss: ['./**/src/scss/**/*.scss',],
 };
 
 /* error */
-var error = function(file, line, column, reason) {
-    console.log(`📄  ${color.blue(`${file}:`)} `);
-    console.log(color.white(` + line ${line}, column ${column}`));
-    console.log(color.white(` + ${reason}`));
+let error = (file, line, column, message) => {
+	log.error(`[${line}:${column}] ${file} →  ${message}`);
 };
 
-/* task:scss */
-gulp.task('scss', gulp.parallel(function(done) {
-    gulp.src(glob.scss, { base: '.' })
-        .pipe(map.init())
-        .pipe(sass({ 
-            outputStyle: 'compressed' 
-        }).on('error', function(err) {
-            console.log(`\n⚠️  ${color.yellow('SCSS Error:')}`);
-            error(err.relativePath, err.line, err.column, err.messageOriginal);
-            gulp.src(glob.scss).pipe(shell('open -a Terminal'));
+/* task : scss */
+gulp.task('scss', gulp.parallel((done) => {
+	gulp.src(watch.scss, { base: '.', })
+		.pipe(map.init())
+		.pipe(sass({ 
+			outputStyle: 'compressed',
+		}).on('error', (err) => {
+			error(err.relativePath, err.line, err.column, err.messageOriginal);
 
-            this.emit('end');
-        }))
-        .pipe(prefix({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(map.write())
-        .pipe(rename(function(path) {
-            path.dirname += '/../css';
-        }))
-        .pipe(gulp.dest(function(file) {
-            return file.base;
-        }))
-        .pipe(reload());
-
-    done();
+			done();
+		}))
+		.pipe(prefix({
+			cascade: false,
+		}))
+		.pipe(map.write('.'))
+		.pipe(rename(function(path) {
+			path.dirname += '/../css';
+		}))
+		.pipe(gulp.dest(function(file) {
+			return file.base;
+		}))
+		.pipe(reload());
+        
+	done();
 }));
 
-/* task:script */
-gulp.task('script', gulp.parallel(function(done) {
-    gulp.src(glob.script)
-        .pipe(jshint())
-        .pipe(jshint.reporter(function(errors) {
-            console.log(`\n⚠️  ${color.yellow('Script Error:')}`);
-            errors.map(function(err) {
-                error(err.file, err.error.line, err.error.character, err.error.reason);
-            });
-            gulp.src(glob.script).pipe(shell('open -a Terminal'));
-        }))
-        .on('error', function(err) {
-            this.emit('end');
-        })
-        .pipe(reload());
+/* task : watch */
+gulp.task('watch', gulp.parallel((done) => {
+	reload.listen({ 'quiet': true, });
 
-    done();
+	gulp.watch(watch.scss).on('change', gulp.parallel(['scss',]));
+	gulp.watch(['**/*.php',]).on('change', reload.changed);
+    
+	done();
 }));
 
-/* task:watch */
-gulp.task('watch', gulp.parallel(function(done) {
-    reload.listen();
-
-    gulp.watch(['*.html', '*.php']).on('change', reload.changed);
-    gulp.watch(glob.scss).on('change', gulp.parallel(['scss']));
-    gulp.watch(glob.script).on('change', gulp.parallel(['script']));
-
-    done();
-}));
-
-/* task:default */
-gulp.task('default', gulp.parallel('watch', 'scss', 'script', function() {
-    console.log(`\n👂🏼  ${color.bold(`Listening for changes...`)}`);
+/* task : default */
+gulp.task('default', gulp.parallel(['watch', 'scss',], (done) => {
+	log.notice('Listening for changes...');
+    
+	done();
 }));
