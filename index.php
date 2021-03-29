f719db5a65685388e58fd649dfa5e58fdf4f0b92
Jon McLean <jon@poweredbyrawon.com>
2021-03-29 10:14:21 +0100

    Updated site

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en-GB">
 
 <head>
     
-    <title>TITLE</title>
-        
+    <title>Mud Test</title>
+    
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-    
+    <meta name="author" content="Jon McLean">
     <link href="src/css/style.css" rel="stylesheet" type="text/css" media="all">
     
 </head>
@@ -16,9 +17,186 @@
 
 <body>
 
-hello
+
+<header>
+    <a href="" class="logo"></a>
+    <nav class="menu">
+        <ul>
+            <li>
+                <a href="">Our network</a>
+            </li>   
+            <li>
+                <a href="" class="dropdown">Who we work with</a>
+                <nav class="submenu">
+                    <ul>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                    </ul>
+                </nav>
+            </li>   
+            <li>
+                <a href="" class="dropdown">Solutions</a>
+                <nav class="submenu">
+                    <ul>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                        <li>item 1</li>
+                    </ul>
+                </nav>
+            </li>   
+            <li>
+                <a href="">News</a>
+            </li>   
+            <li>
+                <a href="">About us</a>
+            </li>  
+            <li>
+                <a href="">Contact us</a>
+            </li>  
+        </ul>
+    </nav>
+    <a href="" class="button login">Login to 1Portal</a>
+    
+
+    <a href="" class="button toggle" data-content="☰">☰</a>
+
+</header>
+
+<main>
+    <section class="intro">
+        <!-- <article style="background-image: url(src/gfx/snailrace.png)"> -->
+        <article>
+            <div class="text">
+                <h1>Bye Bye Legacy.<br> <span>Hello legendary.<span> </h1>
+                <p>Why make do with so-so network solutions, when you can make an impact with us. We challenge conventions of network normality to deliver connectivity that’s better, simpler and more enjoyable to use.  </p>
+                <p class="strapline">Our mission? To help power your business forward - towards tomorrow.</p>
+            </div>
+            <div class="media image" style="background-image: url(src/gfx/snailrace.png)"></div>
+        </article>
+        <a class="button" data-content="Virtual1 for resellers">Virtual1 for resellers</a>
+        <a class="button cyan" data-content="Virtual1 for carriers">Virtual1 for carriers</a>
+
+    </section>
+
+    <section class="ksp">
+        <h4>Enjoy the freedom of our network</h4>
+        <div class="wrapper">
+            <div class="card">
+                <div class="media round"></div>
+                <h5>Selling Point One</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi magnam vero repellat. </p>
+            </div>
+            <div class="card">
+                <div class="media round"></div>
+                <h5>Selling Point Two</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi magnam vero repellat. </p>
+            </div>
+            <div class="card">
+                <div class="media round"></div>
+                <h5>Selling Point Three</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi magnam vero repellat. </p>
+            </div>
+
+        </div>
+
+        <a class="button small" data-content="More about our network">More about our network</a>
+
+    </section>
+
+    <section class="news">
+        <h3>Latest News</h3>
+        <a href="" class="button more">View All</a>
+
+        <div class="wrapper">
+            <div class="card">
+                <div class="frame">
+                    <div class="media image" style="background-image: url(lib/1.jpg)">
+                        <div class="label">News</div>
+                    </div>
+                </div>
+                
+                <div class="meta">25th November 2021 </div>
+                <h4>Where are you on the journey to safe remote work, really?</h4>  
+            </div>
+
+            <div class="card">
+                <div class="frame">
+                    <div class="media image" style="background-image: url(lib/2.jpg)">
+                        <div class="label">News</div>
+                    </div>
+                </div>
+                
+                <div class="meta">25th November 2021 </div>
+                <h4>Virtual1 launches high bandwidth services nationwide</h4> 
+            </div>
+
+            <div class="card">
+                <div class="frame">
+                    <div class="media image" style="background-image: url(lib/3.jpg)">
+                    <div class="label cyan">Blog</div>
+                    </div>
+                </div>
+                
+                <div class="meta">25th November 2021 </div>
+                <h4>How strong is your patch-game?</h4>  
+            </div>
+
+    
+           
+        </div> 
+
+    </section>
+
+    <section class="cta">
+        <div class="wrapper">
+            <h4>Interested in consuming our services?</h4> 
+            <p>Get in touch and we’ll put you in touch with one of registered suppliers.</p>
+            <a class="button" data-content="Find out more">Find out more</a>
+        </div>
+    </section>
+
+    <section class="solutions">
+        <div class="deck">
+            <div class="track">
+                <div class="card active">
+                <div class="media image round" style="background-image: url(src/gfx/data.png"></div>
+                    <h2 class="impact">Connectivity Solutions</h6>  
+                    <p>The route to the internet is about more than just communications; it’s how businesses access the key systems and critical data that they need to operate! </p>
+                    <a class="button" data-content="Read More">Read More</a>
+                </div>
+
+                <div class="card">
+                <div class="media image round" style="background-image: url(src/gfx/cloud.png"></div>
+                    <h2 class="impact">Connectivity Solutions</h6>  
+                    <p>The route to the internet is about more than just communications; it’s how businesses access the key systems and critical data that they need to operate! </p>
+                    <a class="button" data-content="Read More">Read More</a>
+                </div>
+
+                <div class="card">
+                    <div class="media image round" style="background-image: url(src/gfx/cloud.png"></div>
+                    <h2 class="impact">Connectivity Solutions</h6>  
+                    <p>The route to the internet is about more than just communications; it’s how businesses access the key systems and critical data that they need to operate! </p>
+                    <a class="button" data-content="Read More">Read More</a>
+                </div>  
+            </div>
+                <div class="indicators">
+                    <div class="dot active"></div>
+                    <div class="dot"></div>
+                    <div class="dot"></div>
+                </div>
+            </div>
+        </div>
 
 
+    </section>
+
+</main>
+
+<footer>
+</footer>
 
 
 <script src="src/plugin/jquery/jquery.js"></script>
--- src/script/script.js
+++ src/script/script.js
@@ -2,13 +2,91 @@
 $(document).on('ready', init);
 
 /* ----------------------------------------------------------------------- INIT --- */
+function init() {
+    deck_setup();
+    deck_panel_control()
+
+}
+
+/* ----------------------------------------------------------- DECK --- */
+
+function deck_setup() {
+    var $cards;
+    var total;
+    var offset;
+    console.log('running');
+    $('.deck').each(function(i) {
+        $cards = $(this).find('.card');
+        width = $cards.outerWidth();
+        totalwidth = $(window).width();
+        $track = $(this).find('.track');
+        offset = (totalwidth - width) / 2;
+        $track.css("left", offset + "px");
+    });
+    deck_timer();
+}
 
 
-/* ----------------------------------------------------------------PROJECT --- */
+function deck_timer() {;
+    index = 1;
+    setInterval(function() {
+        $deck = $('.deck')
+        $track = $deck.find('.track');
+        $cards = $track.find('.card');
+        $indicators = $('.indicators');
+        $dots = $indicators.find('.dot');
+        total = $cards.length;
+        
+        if (index >= total) {
+            index = 0;
+        } 
+        console.log(index);
+        $dots.removeClass('active');
+        $dots.eq(index).addClass('active');
+        $cards.removeClass('active');
+        $cards.eq(index).addClass('active');
+        
+        offset = $('.card.active').position().left;
+
+        $track.css("transform", "translateX(-" + offset + "px");
+        index++;
+    }, 5000);
+    
+    
+}
 
 
 
+function deck_panel_control() {
+    var $track;
+    var $deck;
+    var $cards;
+    var offset;
+    var current = 0;
+    var $indicators = $('.indicators');
+    var $dots = $indicators.find('.dot');
+    $dots.on("click", function() {
+        $deck = $(this).closest('.deck')
+        $track = $deck.find('.track');
+        $cards = $track.find('.card');
+        
+    
+        index = $(this).index();
+        // indexCard = {current: $(".card.active").index()};
 
+        
+        $dots.removeClass('active');
+        $dots.eq(index).addClass('active');
+        $cards.removeClass('active');
+        $cards.eq(index).addClass('active');
+        
+        offset = $('.card.active').position().left;
+
+        $track.css("transform", "translateX(-" + offset + "px");
+      
+        
+    })
+}
 
 
 
